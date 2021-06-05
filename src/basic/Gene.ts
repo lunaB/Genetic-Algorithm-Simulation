151597621d76ed470a7fe83728db9ea52e66db11
lunaB <luna20617@gmail.com>
2021-06-06 02:19:13 +0900

    feat: Gene

--- src/basic/Gene.ts
+++ src/basic/Gene.ts
@@ -1,3 +1,24 @@
 export class Gene {
-  
+  constructor(
+    public chromosome_size: number,
+    public chromosome_base: Array<number>,
+    public chromosome: Array<any> = []) {
+
+    /* init */
+  }
+
+  initSystem() {}
+
+  initRandom() {
+    for(let i=0;i<this.chromosome_size;i++) {
+      let base = Math.floor(Math.random()*this.chromosome_base.length)
+      this.chromosome.push(this.chromosome_base[base])
+    }
+    this.initSystem()
+  }
+
+  setChromosome(chromosome: Array<any>) {
+    this.chromosome = chromosome
+    this.initSystem()
+  }
 }
